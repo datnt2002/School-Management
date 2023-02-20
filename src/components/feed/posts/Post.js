49a3ce55ef14b201c35ac812f3c9ab1b11783fc8
tlad030702 <tladd2702@gmail.com>
2023-02-20 15:36:56 +0700

    comment textarea

--- src/components/feed/posts/Post.js
+++ src/components/feed/posts/Post.js
@@ -47,13 +47,15 @@ function Post() {
             <hr />
 
             <div className="media mb-2 reply">
-              <Profile imageSrc="https://scontent.fhan14-3.fna.fbcdn.net/v/t1.6435-9/146614516_1768473006657991_2851123883348124585_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_ohc=LUmm1lzU44kAX-qv5s2&tn=7YDAcjGu5PpJ9IVW&_nc_ht=scontent.fhan14-3.fna&oh=00_AfCgbW8g8OCAD_LhNdB0wSyJn2jTpgI82Eexg7lYdTp0YQ&oe=6417F67D" />
+              <Profile 
+                className="mr-2 rounded"
+                height="32"
+                imageSrc="https://scontent.fhan14-3.fna.fbcdn.net/v/t1.6435-9/146614516_1768473006657991_2851123883348124585_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_ohc=LUmm1lzU44kAX-qv5s2&tn=7YDAcjGu5PpJ9IVW&_nc_ht=scontent.fhan14-3.fna&oh=00_AfCgbW8g8OCAD_LhNdB0wSyJn2jTpgI82Eexg7lYdTp0YQ&oe=6417F67D" 
+              />
               <div className="media-body">
-                <Input
-                  type="text"
-                  className="form-control border-0 form-control-sm"
+                <textarea
                   placeholder="Write a comment"
-                ></Input>
+                ></textarea>
               </div>
             </div>
           </div>
