31813341dfb2189bb47c9e28612aae9d263a6939
tlad030702 <tladd2702@gmail.com>
2023-02-20 14:05:01 +0700

    css Input

--- src/components/Tags/Input.js
+++ src/components/Tags/Input.js
@@ -1,9 +1,10 @@
 import { memo } from "react";
+import "./input.css"
 
 function Input({ state, onSetState, placeholder, type, label }) {
   return (
     <div className="form-field">
-      <label>{label} </label>
+      <label>{label}</label>
       <input
         placeholder={placeholder}
         type={type}
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,18 +4,19 @@ import "./header.css";
 
 function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <a className="navbar-brand" href="#">
-          Logo
-        </a>
-        <div className="search-btn">
-          <Input className="form-control" type="search" placeholder="Search" />
-          <button className="btn btn-outline-success" type="submit">
-            Search
-          </button>
-        </div>
-        <a href="#" className="user">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">Logo</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+      <a href="#" className="user">
           <Profile
             className="rounded-circle"
             imageSrc="https://scontent.fhan14-3.fna.fbcdn.net/v/t1.6435-9/146614516_1768473006657991_2851123883348124585_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_ohc=LUmm1lzU44kAX-qv5s2&tn=7YDAcjGu5PpJ9IVW&_nc_ht=scontent.fhan14-3.fna&oh=00_AfCgbW8g8OCAD_LhNdB0wSyJn2jTpgI82Eexg7lYdTp0YQ&oe=6417F67D"
@@ -23,8 +24,8 @@ function Header() {
             userName="Dominic Keller"
           />
         </a>
-      </div>
-    </nav>
+    </div>
+  </nav>
   );
 }
 export default Header;
