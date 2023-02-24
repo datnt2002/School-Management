0e7956c6fce8c5f4d6e4897abe4a59fde050d100
datnt2002 <103309042+datnt2002@users.noreply.github.com>
2023-02-24 14:39:57 +0700

    m

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/header/Header";
 import SubNavAdmin from "./components/subNav/SubNavAdmin";
-import CreateNewCategory from "./pages/Category/CreateNewCategory";
+import Authentication from "./pages/Authentication/Authentication";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SubNavAdmin />
+
+      {/* <Authentication /> */}
     </div>
   );
 }
